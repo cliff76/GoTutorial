0117dba109b455d9f6cfc5e931a3d8544615152c
Clifton Craig <clifton.craig@gmail.com>
2022-08-29 09:07:14 -0700

    Adds error handling.

--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 import "example.com/greetings"
 
 func main() {
-	message := greetings.Hello("Clifton")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	//Request a greeting message.
+	message, err := greetings.Hello("Clifton")
+	//If an error was returned, print it to the console and
+	//exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//If no error was returned, print the message.
 	fmt.Println(message)
 }
